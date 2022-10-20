a78c462d4a2ec97e723da1a536d7366e6a5a12cd
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-20 17:45:55 +0530

    UC3 Completed

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -38,7 +38,59 @@ public class AddressBookMain {
                 case 2:
                     System.out.println(list);
                     break;
+                case 3:
+                    System.out.println("Enter First Name of Contact whose detail you want to edit");
+                    String tempName = sc.next();
+                    editContact(tempName, list);
+
             }
+        }
+    }
+
+    private static void editContact(String tempFirstName, List<Contacts> list) {
+        if (list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).firstName.equals(tempFirstName)) {
+                    System.out.println("\nEnter the Field to Edit \n 1.First Name \n2.Last Name \n3.City \n4.State \n5.Zip" +
+                            "\n6.Phone Number \n7.Email ID");
+                    int choice = sc.nextInt();
+                    switch (choice) {
+                        case 1:
+                            System.out.println("Enter New First Name");
+                            list.get(i).firstName = sc.next();
+                            break;
+                        case 2:
+                            System.out.println("Enter New Last Name");
+                            list.get(i).lastName = sc.next();
+                            break;
+                        case 3:
+                            System.out.println("Enter New City");
+                            list.get(i).city = sc.next();
+                            break;
+                        case 4:
+                            System.out.println("Enter New State");
+                            list.get(i).state = sc.next();
+                            break;
+                        case 5:
+                            System.out.println("Enter New Zip Code");
+                            list.get(i).zip = sc.next();
+                            break;
+                        case 6:
+                            System.out.println("Enter New Phone Number");
+                            list.get(i).phonenumber = sc.next();
+                            break;
+                        case 7:
+                            System.out.println("Enter New Email Id");
+                            list.get(i).email = sc.next();
+                            break;
+                        default:
+                            System.out.println("Invalid Selection");
+                            break;
+                    }
+                }
+            }
+        } else {
+            System.out.println("Contacts not Found...");
         }
     }
 
@@ -47,6 +99,7 @@ public class AddressBookMain {
         System.out.println("0. exit.");
         System.out.println("1. Add New Contact.");
         System.out.println("2. View Contact.");
+        System.out.println("3. Edit Contact");
         return sc.nextInt();
     }
 }
--- Contacts.java
+++ Contacts.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Contacts {
     String firstName;
     String lastName;
